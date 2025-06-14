50da0f36a3b186cba90f9536acdb843786b51a29
Chyrus Miguel D. Macalla <macalla.chyrusmiguel@gmail.com>
2025-06-14 13:49:50 +0800

    Original code

---
+++ hangman_words.py
@@ -0,0 +1,14 @@
+# This file contains the words that will be used.
+
+vegetables_words = (
+    "tomato", "cabbage", "onion", "cucumber", "cauliflower", 
+    "carrot", "broccoli", "spinach", "potato", "lettuce", 
+    "peas", "zucchini", "pumpkin", "radish", "eggplant"
+)
+
+fruits_words = (
+    "apple", "banana", "orange", "grapes", "mango", 
+    "pineapple", "strawberry", "blueberry", "watermelon", "peach", 
+    "cherry", "pomegranate", "kiwi", "papaya", "guava", 
+    "lemon", "lychee", "raspberry", "blackberry", "coconut", "guava"
+)
---
+++ main.py
@@ -0,0 +1,70 @@
+import random
+from hangman_words import fruits_words
+from hangman_art import hangman_art
+
+# Display the hangman and print out from Hangman_Art
+def display_man(wrong_guesses):
+    print("________________________")
+    for line in hangman_art[wrong_guesses]:
+        print(line)
+    print("________________________")
+
+# Print out the hit likt this "_ _ _ _"
+def display_hint(hint):
+    print(" ".join(hint))
+
+# Shows answer
+def display_answer(answer):
+    print(" ".join(answer))
+
+def main():
+    answer = random.choice(fruits_words)
+    hint = ["_"] * len(answer)
+    wrong_guesses = 0
+    guessed_letters = set()
+    is_running = True
+    
+    while is_running:
+        display_man(wrong_guesses)
+        display_hint(hint)
+        guess = input("Enter a letter: ").lower()
+        
+        if len(guess) > 1 or not guess.isalpha():
+            print("Invalid Input.")
+            continue
+        
+        if guess in guessed_letters:
+            print(f"{guess} is already guessed.")
+            continue
+        
+        guessed_letters.add(guess)
+        
+        if guess in answer:
+            for i in range(len(answer)):
+                if answer[i] == guess:
+                    hint[i] = guess
+        else:
+            wrong_guesses += 1
+            
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("Yay! You saved the man!")
+            is_running = False
+        elif wrong_guesses >= len(hangman_art) - 1:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("Man died☠️ (What have you done!?)")
+            is_running = False
+            
+if __name__ == '__main__':
+    main()
+    
+
+play_again = input("Play Again? (Y/N): ").upper()
+
+while play_again != "N":
+    main()
+    play_again = input("Play Again? (Y/N): ").upper()
+    
+exit("Goodbye!")
